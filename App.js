839325b6226106c0eb157e9fbafba30a1cd0427a
Douglas Nassif Roma Junior <douglas.junior@db1.com.br>
2023-07-18 18:53:30 -0300

    AULA: adiciona exemplo de componentes

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StatusBar, Text, View, SafeAreaView,
+  Image, TextInput, ScrollView, PixelRatio
+} from 'react-native';
+
+
+
+import { Button, Switch, Avatar } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <SafeAreaView>
+        <ScrollView>
+          <StatusBar translucent={false} backgroundColor="#ff00ff" />
+          <Text
+            style={{
+              fontSize: 48,
+            }}
+          >
+            Olá Expo!
+            {' '}
+            {PixelRatio.get()}
+          </Text>
+
+          <Image
+            source={{
+              uri: 'https://img.freepik.com/fotos-gratis/paisagem-de-nevoeiro-matinal-e-montanhas-com-baloes-de-ar-quente-ao-nascer-do-sol_335224-794.jpg'
+            }}
+            style={{
+              height: 300,
+              width: 600
+            }}
+          />
+
+          <TextInput
+            placeholder='Informe seu nome'
+            style={{
+              fontSize: 32,
+              borderWidth: 1,
+              borderColor: 'black',
+              padding: 8,
+              margin: 8,
+              borderRadius: 8,
+            }}
+          />
+
+          <Button
+            mode='contained'
+            onPress={() => alert('Olá, mundo!')}
+          >
+            Clique aqui!
+          </Button>
+
+          <Switch value={true} />
+
+          <Avatar.Icon icon="camera" size={200} />
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
